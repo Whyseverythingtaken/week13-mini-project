4b61149a60738bd4a3f1ba337e0d78640375b18c
Hamed Ali <123hamed@gmail.com>
2022-07-02 13:47:14 +0100

    add on to many relations

--- models/index.js
+++ models/index.js
@@ -2,4 +2,20 @@ const Traveller = require("./Traveller");
 const Location = require("./Location");
 const Trip = require("./Trip");
 
+Trip.hasMany(Traveller, {
+  foreignKey: "traveller_id",
+});
+
+Traveller.belongsTo(Trip, {
+  foreignKey: "traveller_id",
+});
+
+Trip.hasMany(Location, {
+  foreignKey: "location_id",
+});
+
+Location.belongsTo(Trip, {
+  foreignKey: "location_id",
+});
+
 module.exports = { Traveller, Location, Trip };
